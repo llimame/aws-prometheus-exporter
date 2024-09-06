520e248da2b50e6c07faf895b8583e3e608931c8
Rodrigo Lima <rodrigo.lima@samplemed.com.br>
2024-09-06 15:30:06 -0300

    feat: add ec2 reserved instances

--- exporters/ec2_exporter.py
+++ exporters/ec2_exporter.py
@@ -1,25 +1,67 @@
 import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 from prometheus_client import Gauge
 from utils.aws_context import ACCOUNT_ID
 
-# Prometheus Gauges
+# Prometheus Gauges for EC2 Instances
 ec2_instances_gauge = Gauge('aws_ec2_instances_total', 'Total number of EC2 instances', ['account_id', 'region'])
 ec2_instance_types_gauge = Gauge('aws_ec2_instance_types', 'Number of EC2 instances by type', ['account_id', 'region', 'instance_type'])
+
+# New Gauge for Reserved Instances
+reserved_instances_gauge = Gauge('aws_ec2_reserved_instances_total', 'Total number of Reserved EC2 Instances', ['account_id', 'region'])
+reserved_instance_types_gauge = Gauge('aws_ec2_reserved_instance_types', 'Number of Reserved EC2 Instances by type', ['account_id', 'region', 'instance_type'])
 
 def collect_ec2_metrics(region='us-east-1'):
     ec2_client = boto3.client('ec2', region_name=region)
 
-    instances = ec2_client.describe_instances()['Reservations']
-    total_instances = sum(len(reservation['Instances']) for reservation in instances)
-    ec2_instances_gauge.labels(account_id=ACCOUNT_ID, region=region).set(total_instances)
+    try:
+        paginator = ec2_client.get_paginator('describe_instances')
+        total_instances = 0
+        instance_type_count = {}
 
-    instance_type_count = {}
-    for reservation in instances:
-        for instance in reservation['Instances']:
-            instance_type = instance['InstanceType']
-            if instance_type not in instance_type_count:
-                instance_type_count[instance_type] = 0
-            instance_type_count[instance_type] += 1
+        # Handle paginated responses for EC2 instances
+        for page in paginator.paginate():
+            instances = page['Reservations']
+            total_instances += sum(len(reservation['Instances']) for reservation in instances)
 
-    for instance_type, count in instance_type_count.items():
-        ec2_instance_types_gauge.labels(account_id=ACCOUNT_ID, region=region, instance_type=instance_type).set(count)
+            for reservation in instances:
+                for instance in reservation['Instances']:
+                    instance_type = instance['InstanceType']
+                    if instance_type not in instance_type_count:
+                        instance_type_count[instance_type] = 0
+                    instance_type_count[instance_type] += 1
+
+        # Update Prometheus Gauges for EC2 instances
+        ec2_instances_gauge.labels(account_id=ACCOUNT_ID, region=region).set(total_instances)
+        for instance_type, count in instance_type_count.items():
+            ec2_instance_types_gauge.labels(account_id=ACCOUNT_ID, region=region, instance_type=instance_type).set(count)
+
+    except (BotoCoreError, ClientError) as e:
+        print(f"Error fetching EC2 metrics: {e}")
+
+def collect_reserved_instance_metrics(region='us-east-1'):
+    ec2_client = boto3.client('ec2', region_name=region)
+
+    try:
+        reserved_instances = ec2_client.describe_reserved_instances(Filters=[{'Name': 'state', 'Values': ['active']}])['ReservedInstances']
+        total_reserved_instances = len(reserved_instances)
+
+        reserved_instance_type_count = {}
+        for reserved_instance in reserved_instances:
+            instance_type = reserved_instance['InstanceType']
+            if instance_type not in reserved_instance_type_count:
+                reserved_instance_type_count[instance_type] = 0
+            reserved_instance_type_count[instance_type] += reserved_instance['InstanceCount']
+
+        # Update Prometheus Gauges for Reserved Instances
+        reserved_instances_gauge.labels(account_id=ACCOUNT_ID, region=region).set(total_reserved_instances)
+        for instance_type, count in reserved_instance_type_count.items():
+            reserved_instance_types_gauge.labels(account_id=ACCOUNT_ID, region=region, instance_type=instance_type).set(count)
+
+    except (BotoCoreError, ClientError) as e:
+        print(f"Error fetching Reserved Instance metrics: {e}")
+
+# Call both metric collectors
+def collect_all_metrics(region='us-east-1'):
+    collect_ec2_metrics(region)
+    collect_reserved_instance_metrics(region)
