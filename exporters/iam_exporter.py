15d01e37c79d4e6d8b2bfd086b71014c0cbb4782
Rodrigo Lima <rodrigo.lima@samplemed.com.br>
2024-09-06 15:04:57 -0300

    feat: add non used keys in the last 30 dyas

--- exporters/iam_exporter.py
+++ exporters/iam_exporter.py
@@ -1,27 +1,59 @@
 import boto3
 import time
 from prometheus_client import Gauge
-from utils.aws_context import ACCOUNT_ID  # Importando o ID da conta
+from utils.aws_context import ACCOUNT_ID  # Importing the account ID
+from botocore.exceptions import ClientError
 
 iam_client = boto3.client('iam')
 
 # Prometheus Gauges
 iam_users_gauge = Gauge('aws_iam_users_total', 'Total number of IAM users', ['account_id'])
 iam_access_keys_gauge = Gauge('aws_iam_access_keys_old', 'Number of IAM access keys older than X days', ['account_id'])
+iam_unused_keys_gauge = Gauge('aws_iam_access_keys_unused', 'Number of IAM access keys not used in the last 30 days', ['account_id'])  # Renamed metric
 
-# Set the threshold for IAM access keys age
+# Set the threshold for IAM access keys age and usage
 ACCESS_KEY_AGE_THRESHOLD = 90
+ACCESS_KEY_UNUSED_THRESHOLD = 30
 
 def collect_iam_metrics():
-    users = iam_client.list_users()['Users']
-    iam_users_gauge.labels(account_id=ACCOUNT_ID).set(len(users))
+    try:
+        old_keys_count = 0
+        unused_keys_count = 0
+        users = []
+        
+        # Paginate through all IAM users
+        paginator = iam_client.get_paginator('list_users')
+        for page in paginator.paginate():
+            users.extend(page['Users'])
+        
+        # Set the total number of IAM users in Prometheus gauge
+        iam_users_gauge.labels(account_id=ACCOUNT_ID).set(len(users))
+        
+        # Loop through users and get their access keys
+        for user in users:
+            access_key_paginator = iam_client.get_paginator('list_access_keys')
+            for key_page in access_key_paginator.paginate(UserName=user['UserName']):
+                for key in key_page['AccessKeyMetadata']:
+                    age = (time.time() - key['CreateDate'].timestamp()) / (60 * 60 * 24)
+                    if age > ACCESS_KEY_AGE_THRESHOLD:
+                        old_keys_count += 1
+                    
+                    # Get the last used date for each key
+                    last_used_response = iam_client.get_access_key_last_used(AccessKeyId=key['AccessKeyId'])
+                    last_used_date = last_used_response.get('AccessKeyLastUsed', {}).get('LastUsedDate')
+                    
+                    # Check if the key has been used in the last 30 days
+                    if last_used_date:
+                        days_since_last_used = (time.time() - last_used_date.timestamp()) / (60 * 60 * 24)
+                        if days_since_last_used > ACCESS_KEY_UNUSED_THRESHOLD:
+                            unused_keys_count += 1
+                    else:
+                        # If there is no record of the key being used, it counts as unused
+                        unused_keys_count += 1
 
-    old_keys_count = 0
-    for user in users:
-        access_keys = iam_client.list_access_keys(UserName=user['UserName'])['AccessKeyMetadata']
-        for key in access_keys:
-            age = (time.time() - key['CreateDate'].timestamp()) / (60 * 60 * 24)
-            if age > ACCESS_KEY_AGE_THRESHOLD:
-                old_keys_count += 1
+        # Set the number of old access keys and unused access keys in Prometheus gauges
+        iam_access_keys_gauge.labels(account_id=ACCOUNT_ID).set(old_keys_count)
+        iam_unused_keys_gauge.labels(account_id=ACCOUNT_ID).set(unused_keys_count)
 
-    iam_access_keys_gauge.labels(account_id=ACCOUNT_ID).set(old_keys_count)
+    except ClientError as e:
+        print(f"Error fetching IAM metrics: {e}")
