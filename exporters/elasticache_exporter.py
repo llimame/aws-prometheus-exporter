a09346c8c14f00c8c2dfa6f5b478ca4cae20d3f9
Rodrigo Lima <rodrigo.lima@samplemed.com.br>
2024-09-19 08:31:06 -0300

    feat: elasticache reserved instances

--- exporters/elasticache_exporter.py
+++ exporters/elasticache_exporter.py
@@ -3,17 +3,16 @@ from prometheus_client import Gauge
 from utils.aws_context import ACCOUNT_ID
 
 # Prometheus Gauges
-elasticache_clusters_gauge = Gauge('aws_elasticache_clusters_total', 'Total number of ElastiCache clusters', ['account_id', 'region'])
 elasticache_cluster_types_gauge = Gauge('aws_elasticache_cluster_types', 'Number of ElastiCache clusters by type', ['account_id', 'region', 'cluster_type'])
+reserved_cache_node_types_gauge = Gauge('aws_elasticache_reserved_node_types', 'Number of reserved ElastiCache nodes by type', ['account_id', 'region', 'instance_type'])
 
 def collect_elasticache_metrics(region='us-east-1'):
     elasticache_client = boto3.client('elasticache', region_name=region)
 
     # Get cluster information
     clusters = elasticache_client.describe_cache_clusters()['CacheClusters']
-    total_clusters = len(clusters)
-    elasticache_clusters_gauge.labels(account_id=ACCOUNT_ID, region=region).set(total_clusters)
-
+    
+    # Count clusters by type
     cluster_type_count = {
         'redis': 0,
         'memcached': 0
@@ -25,3 +24,17 @@ def collect_elasticache_metrics(region='us-east-1'):
 
     for cluster_type, count in cluster_type_count.items():
         elasticache_cluster_types_gauge.labels(account_id=ACCOUNT_ID, region=region, cluster_type=cluster_type).set(count)
+
+    # Get reserved instance information
+    reserved_nodes = elasticache_client.describe_reserved_cache_nodes()['ReservedCacheNodes']
+
+    # Count reserved instances by node type
+    reserved_node_type_count = {}
+    for node in reserved_nodes:
+        instance_type = node['CacheNodeType']
+        if instance_type not in reserved_node_type_count:
+            reserved_node_type_count[instance_type] = 0
+        reserved_node_type_count[instance_type] += 1
+
+    for instance_type, count in reserved_node_type_count.items():
+        reserved_cache_node_types_gauge.labels(account_id=ACCOUNT_ID, region=region, instance_type=instance_type).set(count)
